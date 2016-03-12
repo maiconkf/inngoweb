148eee2e7223654cd21379e6c67ea5506d63c751
Maicon Furtado <maiconfurtado@brave,ag>
2016-03-12 00:33:26 -0300

    terms and privacy

---
+++ tpls/terms-of-conditions.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	    <meta charset="UTF-8">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	    <title>Inn & Go</title>
+	    <meta name="viewport" content="width=1200, initial-scale=1">
+	    <link rel="stylesheet" href="../dist/css/styles.min.css">
+	</head>
+	<body id="layout-two">
+		<?php include_once('includes/menu.php'); ?>
+		<div id="intern" class="clearfix">
+			<div class="container">
+				<div class="col m7 center-block">
+					<h1 class="f60 hd-medium center-align">Terms and Conditions</h1>
+					<div class="text">
+						<p>
+							Each Member purchases one week in high season which remains the same each year for the duration of the lease. Each Member is also entitled to four planned vacation weeks throughout the year which are chosen several months in advance. To avoid conflicts, an equitable rotation system is used. The lease is for a period of 75 years and Members can sell or transfer their membership anytime.
+						</p>
+						<p>
+							For any weeks that are not occupied by the Member, friends or family, these can be released back to the hotel for rental. Each member receives 60% of the rental and this money is credited to the Club Member’s account. This money can be used to pay for extra's incurred at the hotel (car rental, flights, bar and restaurant bills, etc.), pay the quarterly due fees or can be credited back to the Member.
+						</p>
+						<h2 class="hd-medium f30">1.0 Terms of Use</h2>
+						<p>
+							The Club provides a full time concierge service, to arrange flights to and from Saint-Barthélemy, VIP Service, shopping delivered to the villa prior to arrival, babysitting, restaurant reservations, car rental, in-villa personal coach, etc.
+						</p>
+						<p>
+							The Club also provides storage for all Members. Personal belongings can be left in special containers in the hotel’s secure air-conditioned storage facility. The boxes will be delivered to the villa prior to arrival and taken away at the end of the stay. Beach Club: umbrellas, beach towels, snorkelling gear, water activities, beverage services and reserved beach chairs.
+						</p>
+						<h2 class="hd-medium f30">2.0 Restrictions for Direction</h2>
+						<p>
+							The Club provides a full time concierge service, to arrange flights to and from Saint-Barthélemy, VIP Service, shopping delivered to the villa prior to arrival, babysitting, restaurant reservations, car rental, in-villa personal coach, etc.
+						</p>
+						<p>
+							The Club also provides storage for all Members. Personal belongings can be left in special containers in the hotel’s secure air-conditioned storage facility. The boxes will be delivered to the villa prior to arrival and taken away at the end of the stay. Beach Club: umbrellas, beach towels, snorkelling gear, water activities, beverage services and reserved beach chairs.
+						</p>
+						<h2 class="hd-medium f30">3.0 Conditions for Hosts</h2>
+						<p>
+							The Club provides a full time concierge service, to arrange flights to and from Saint-Barthélemy, VIP Service, shopping delivered to the villa prior to arrival, babysitting, restaurant reservations, car rental, in-villa personal coach, etc.						</p>
+						</p>
+						<h2 class="hd-medium f30">4.0 Magic</h2>
+						<p>
+							The Club provides a full time concierge service, to arrange flights to and from Saint-Barthélemy, VIP Service, shopping delivered to the villa prior to arrival, babysitting, restaurant reservations, car rental, in-villa personal coach, etc.						</p>
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<?php include_once('includes/footer.php'); ?>
+		<script src="../dist/js/scripts.min.js"></script>
+	</body>
+</html>
