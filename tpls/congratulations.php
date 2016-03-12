2a16889fafb203f36dc1813662a2157f597c7422
Maicon Furtado <maiconfurtado@brave,ag>
2016-03-12 16:07:26 -0300

    congratulations

---
+++ tpls/congratulations.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	    <meta charset="UTF-8">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	    <title>Inn & Go</title>
+	    <meta name="viewport" content="width=1200, initial-scale=1">
+	    <link rel="stylesheet" href="../dist/css/styles.min.css">
+	</head>
+	<body id="layout-two" class="no-booking">
+		<?php include_once('includes/menu.php'); ?>
+		<div id="intern" class="clearfix">
+			<div class="congratulations container clearfix">
+				<h1 class="f145 hd-medium center-align">Congratulations</h1>
+				<div class="col m6 center-block">
+					<div class="col m11">
+						<p class="f18 a-light center-align">
+							Well-appointed with queen or twin beds our guestrooms fully carpeted, high speed wired internet, international or local direct dial, mini bar, color television with satellite programmes, 24 hours in- room dining, laundry service, safety deposit box.
+						</p>
+						<a href="javascript:;" class="btn">Go to the Home -></a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<?php include_once('includes/footer.php'); ?>
+		<script src="../dist/js/scripts.min.js"></script>
+	</body>
+</html>
